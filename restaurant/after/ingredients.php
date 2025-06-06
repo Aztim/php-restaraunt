089323f28f5012b756c6e579e893407da4f88e59
Aztim <barboza-82@mail.ru>
2025-06-06 23:50:17 +0300

    added if-condition for header and title tags in header.php

--- restaurant/after/ingredients.php
+++ restaurant/after/ingredients.php
@@ -1,6 +1,7 @@
 <?php 
-  $pageTitle = 'Ingridients';
-  $headerImg = 'images/pexels-julia-volk-5273044.jpg';
+  // $pageTitle = 'Ingredients';
+  $pageKey = 'ingredients';
+  // $headerImg = 'images/pexels-julia-volk-5273044.jpg';
   include 'inc/header.inc.php'; 
   
   ?>
